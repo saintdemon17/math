6c33bdead0bf1943418db0ce6fbf7ea524dc247d
saintdemon17 <sement7sev@gmail.com>
2026-04-26 19:54:03 +0300

    Added encoding

--- file_actions.py
+++ file_actions.py
@@ -1,5 +1,5 @@
 # Открыть на запись файл example.txt
-file = open('example.txt', 'w')
+file = open('example.txt', 'w', encoding='utf-8')
 # Записать в файл строку.
 file.write('Зевну, укроюсь с головою,\nбудильник заведу на март.\n')
 # Закрыть файл.
